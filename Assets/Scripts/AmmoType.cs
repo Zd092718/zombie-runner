b8e94ae655bb04aebf8dbc27d26ba555131c385e
Zachary Dowd <zacharyd2796@zohomail.com>
2022-05-11 16:17:15 -0600

    ammo types in place

---
+++ Assets/Scripts/AmmoType.cs
@@ -0,0 +1,7 @@
+public enum AmmoType
+{
+    Rounds,
+    Shells,
+    Bullets
+
+}
--- Assets/Scripts/Weapon/Weapon.cs
+++ Assets/Scripts/Weapon/Weapon.cs
@@ -11,10 +11,15 @@ public class Weapon : MonoBehaviour
     [SerializeField] ParticleSystem muzzleFlash;
     [SerializeField] GameObject hitEffect;
     [SerializeField] Ammo ammoSlot;
+    [SerializeField] AmmoType ammoType;
     [SerializeField] float timeBetweenShots = 0.5f;
 
     bool canShoot = true;
 
+    private void OnEnable()
+    {
+        canShoot = true;
+    }
     void Update()
     {
         if (Input.GetMouseButtonDown(0) && canShoot == true)
@@ -26,12 +31,12 @@ public class Weapon : MonoBehaviour
     IEnumerator Shoot()
     {
         canShoot = false;
-        if (ammoSlot.GetCurrentAmmo() > 0)
+        if (ammoSlot.GetCurrentAmmo(ammoType) > 0)
         {
 
             PlayMuzzleFlash();
             ProcessRayCast();
-            ammoSlot.ReduceCurrentAmmo();
+            ammoSlot.ReduceCurrentAmmo(ammoType);
         }
         yield return new WaitForSeconds(timeBetweenShots);
         canShoot = true;
